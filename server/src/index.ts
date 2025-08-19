c1c6d8f4478444488cd3a451039bf05209b63dcd
Monika-Choudhary <monikachahota@gmail.com>
2025-08-19 12:24:15 +0200

    fix: Correct import paths and uncomment route middleware in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
-// import { connect } from "./utils/db.js.js";
+import { connect } from "./utils/db.js";
 import express from "express";
 import config from "./config/config.js";
 import { errorHandling } from "./middleware/errorHandling.js";
-// import authRoutes from "./routes/authRouter.js.js";
-// import userRoutes from "./routes/userRouter.js.js";
+import authRoutes from "./routes/authRouter.js";
+import userRoutes from "./routes/userRouter.js";
 import cookieParser from "cookie-parser";
 
 import cors from "cors";
@@ -32,8 +32,8 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-// app.use("/api/auth", authRoutes);
-// app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 // Beispiel-Route
 app.get("/", (_req, res) => {
@@ -42,7 +42,7 @@ app.get("/", (_req, res) => {
 
 app.use(errorHandling);
 
-// connect();
+connect();
 app.listen(config.PORT, () =>
   console.log(`Server is running on PORT http//localhost:${config.PORT}`)
 );
