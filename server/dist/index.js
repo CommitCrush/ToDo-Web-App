01970798551c67e9b5361f08b391ca23a5eb455a
Monika-Choudhary <monikachahota@gmail.com>
2025-08-19 13:05:59 +0200

    feat: Implement user authentication and authorization with JWT, including registration, login, and profile management

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,9 +1,9 @@
-// import { connect } from "./utils/db.js.js";
+import { connect } from "./utils/db.js";
 import express from "express";
 import config from "./config/config.js";
 import { errorHandling } from "./middleware/errorHandling.js";
-// import authRoutes from "./routes/authRouter.js.js";
-// import userRoutes from "./routes/userRouter.js.js";
+import authRoutes from "./routes/authRouter.js";
+import userRoutes from "./routes/userRouter.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 const allowedOrigins = [
@@ -26,13 +26,13 @@ app.use(cors({
 app.use(express.json());
 //app.use(cors());
 app.use(cookieParser());
-// app.use("/api/auth", authRoutes);
-// app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 // Beispiel-Route
 app.get("/", (_req, res) => {
     res.json({ message: "Hello from backend" });
 });
 app.use(errorHandling);
-// connect();
+connect();
 app.listen(config.PORT, () => console.log(`Server is running on PORT http//localhost:${config.PORT}`));
 //admin123!&/MN1231asc
