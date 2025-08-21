5f6ee73cae7575731d54155f4e2b670a0da6208b
Ese-tech <ese.osagie19@gmail.com>
2025-08-21 09:59:41 +0200

    feat: Update allowedOrigins to restrict CORS to production frontend URL

--- server/dist/index.js
+++ server/dist/index.js
@@ -8,8 +8,7 @@ import todoRoutes from "./routes/todoRouter.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:3000",
+    "https://todo-web-app-frontend-be7s.onrender.com",
 ];
 const app = express();
 app.use(cors({
--- server/src/index.ts
+++ server/src/index.ts
@@ -10,8 +10,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:3000",
+  "https://todo-web-app-frontend-be7s.onrender.com",
 ];
 const app = express();
 
